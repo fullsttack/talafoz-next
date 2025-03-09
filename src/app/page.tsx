716ae6cae11d60d2193fb4d2bf6523f30d6852d0
Mohsen <61712887+fullsttack@users.noreply.github.com>
2025-03-09 14:29:43 +0330

    ff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import SubCart from "@/components/subscription/SubCart";
 import LastBlog from "@/components/blog/LastBlog";
 import TechBanner from "@/components/site/TechBanner";
 import Footer from "@/components/layout/Footer";
+import { MarqueeDemo } from "@/components/magicui/marquee";
 export default function Home() {
   return (
     <div className="overflow-x-hidden flex flex-col min-h-screen">
@@ -18,6 +19,7 @@ export default function Home() {
         <OfferCourse />
         <SubCart />
         <LastBlog />
+        <MarqueeDemo />
         <TechBanner />
       </div>
       <Footer />
