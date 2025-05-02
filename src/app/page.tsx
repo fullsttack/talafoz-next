fa4d23f24b30a8e10acfbf2b0e7115df82701683
Mohsen <61712887+fullsttack@users.noreply.github.com>
2025-05-02 07:12:12 +0330

    ب

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import BestCourse from "@/components/layouts/BestCourse";
 import AboutIndex from "@/components/layouts/AboutIndex";
 import BlogList from "@/components/layouts/BlogList";
 import FaqIndex from "@/components/layouts/FaqIndex";
-
+import Footer from "@/components/layouts/Footer";
 
 
 export default function Home() {
@@ -18,7 +18,7 @@ export default function Home() {
     <AboutIndex />
     <BlogList />
     <FaqIndex />
-
+    <Footer />
    </div>
   );
 }
