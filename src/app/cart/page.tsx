ef2f501145dff8ac937d0f8bb696797b57ea6740
Mohsen <61712887+fullsttack@users.noreply.github.com>
2025-05-02 06:02:01 +0330

    fix

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+export default function CartPage() {
+  return (
+    <div className="min-h-[80vh] flex flex-col items-center justify-center text-gray-600 dark:text-gray-300">
+      <Image src="/vector/cart.svg" alt="cart" className="" width={500} height={500} />
+      <h2 className="mt-6 font-medium text-xl">سبد خرید شما خالی است</h2>
+      <p className="mt-2 text-gray-500 dark:text-gray-600">محصولی به سبد خرید خود اضافه نکرده‌اید.</p>
+    </div>
+  );
+}
